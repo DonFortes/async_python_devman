e831e91cce4aac39e9a23078f888f1402e9a253a
Павел <paul-36@mail.ru>
2022-05-27 16:13:58 +0300

    Ninth lesson completed: sky full of randomly shining stars.

--- main.py
+++ main.py
@@ -1,72 +1,89 @@
+import asyncio
+import random
 import time
 import curses
+from functools import partial
 
 
-class EventLoopCommand:
+STARS_COUNT = 200
+MIN_OFFSET_TIME = 0.0001
+MAX_OFFSET_TIME = 0.01
+
+
+class Sleep:
+    def __init__(self, seconds):
+        self.seconds = seconds
+
     def __await__(self):
         return (yield self)
 
 
-class Sleep(EventLoopCommand):
-    def __init__(self, seconds):
-        self.seconds = seconds
-
-
-async def blink(canvas, row, column, symbol='*'):
+async def blink(canvas, row, column, symbol="*", offset=0):
     while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        await Sleep(2)
+        if offset == 0:
+            canvas.addstr(row, column, symbol, curses.A_DIM)
+            await Sleep(0.5)
+            offset += 1
 
-        canvas.addstr(row, column, symbol)
-        await Sleep(0.3)
+        else:
+            time.sleep(random.uniform(MIN_OFFSET_TIME, MAX_OFFSET_TIME))
+            canvas.addstr(row, column, symbol)
+            await Sleep(0.3)
 
-        canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await Sleep(0.5)
+            time.sleep(random.uniform(MIN_OFFSET_TIME, MAX_OFFSET_TIME))
+            canvas.addstr(row, column, symbol, curses.A_BOLD)
+            await Sleep(0.5)
 
-        canvas.addstr(row, column, symbol)
-        await Sleep(0.3)
+            time.sleep(random.uniform(MIN_OFFSET_TIME, MAX_OFFSET_TIME))
+            canvas.addstr(row, column, symbol)
+            await Sleep(0.3)
+
+            time.sleep(random.uniform(MIN_OFFSET_TIME, MAX_OFFSET_TIME))
+            canvas.addstr(row, column, symbol, curses.A_DIM)
+            await Sleep(0.5)
 
 
-def draw(canvas):
+def make_coroutines(canvas) -> list:
+    """Make a list with coroutines."""
+
+    border_limits: tuple = canvas.getmaxyx()
+    x, y = tuple(partial(random.randint, 1, limit - 2) for limit in border_limits)
+    symbols = "+*.:"
+    coroutines = [
+        blink(canvas, x(), y(), random.choice(symbols)) for _ in range(STARS_COUNT)
+    ]
+    return coroutines
+
+
+def draw(canvas) -> None:
+    """Draw the stars."""
+
     canvas.refresh()
     canvas.border()
-    coroutines = [blink(canvas, 5, i) for i in range(5, 26, 5)]
-
+    coroutines = make_coroutines(canvas)
     time_to_wait: int or float = 0.5  # default value, but it will change further
+
     while True:
         for coroutine in coroutines.copy():
             try:
                 wait = coroutine.send(None)
-                time_to_wait = wait.seconds
-                canvas.refresh()
             except StopIteration:
                 coroutines.remove(coroutine)
-                print('That is all')
-                time.sleep(2)
+                print("That is all")
+            else:
+                time_to_wait = wait.seconds
+                canvas.refresh()
+
         time.sleep(time_to_wait)
 
         if len(coroutines) == 0:
-            print('Len == 0')
+            print("Len == 0")
             time.sleep(2)
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     curses.update_lines_cols()
     curses.initscr()
     curses.curs_set(0)
     curses.wrapper(draw)
-
-    # while True:
-    #     canvas.addstr(row, column, '*', curses.A_DIM)
-    #     time.sleep(2)
-    #     canvas.refresh()
-    #     canvas.addstr(row, column, '*')
-    #     time.sleep(0.3)
-    #     canvas.refresh()
-    #     canvas.addstr(row, column, '*', curses.A_BOLD)
-    #     time.sleep(0.5)
-    #     canvas.refresh()
-    #     canvas.addstr(row, column, '*')
-    #     time.sleep(0.3)
-    #     canvas.refresh()
