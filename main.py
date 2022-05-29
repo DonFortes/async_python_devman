1af767bd0e762827226a0341c585ae2d38308d09
Павел <paul-36@mail.ru>
2022-05-29 23:21:03 +0300

    Tenth lesson complete. Rewrite with OOP. Use await to skip a step instead of time.sleep.

--- main.py
+++ main.py
@@ -1,89 +1,154 @@
 import asyncio
-import random
 import time
-import curses
 from functools import partial
+from typing import Coroutine
+import random
+import curses
 
 
 STARS_COUNT = 200
-MIN_OFFSET_TIME = 0.0001
-MAX_OFFSET_TIME = 0.01
+TIC_TIMEOUT = 0.1
 
 
-class Sleep:
-    def __init__(self, seconds):
-        self.seconds = seconds
-
-    def __await__(self):
-        return (yield self)
+async def wait_for_queue(tics):
+    """Function that will be used in case when coroutine needs to skip a step."""
+    for _ in range(tics):
+        await asyncio.sleep(0)
 
 
-async def blink(canvas, row, column, symbol="*", offset=0):
-    while True:
-        if offset == 0:
-            canvas.addstr(row, column, symbol, curses.A_DIM)
-            await Sleep(0.5)
-            offset += 1
+class SpaceObject:
+    """Factory class for space objects."""
 
-        else:
-            time.sleep(random.uniform(MIN_OFFSET_TIME, MAX_OFFSET_TIME))
-            canvas.addstr(row, column, symbol)
-            await Sleep(0.3)
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.border_limits: tuple = self.canvas.getmaxyx()
 
-            time.sleep(random.uniform(MIN_OFFSET_TIME, MAX_OFFSET_TIME))
-            canvas.addstr(row, column, symbol, curses.A_BOLD)
-            await Sleep(0.5)
+    async def animate(self, *args, **kwargs) -> None:
+        """Method to animate each object."""
+        return None
 
-            time.sleep(random.uniform(MIN_OFFSET_TIME, MAX_OFFSET_TIME))
-            canvas.addstr(row, column, symbol)
-            await Sleep(0.3)
-
-            time.sleep(random.uniform(MIN_OFFSET_TIME, MAX_OFFSET_TIME))
-            canvas.addstr(row, column, symbol, curses.A_DIM)
-            await Sleep(0.5)
+    def create_event_loop(self) -> list[Coroutine]:
+        """Method to create event loop of each space object.
+        It always returns a list with coroutines."""
+        return []
 
 
-def make_coroutines(canvas) -> list:
-    """Make a list with coroutines."""
+class SkyStar(SpaceObject):
+    """Class to make the sky full of stars."""
 
-    border_limits: tuple = canvas.getmaxyx()
-    x, y = tuple(partial(random.randint, 1, limit - 2) for limit in border_limits)
-    symbols = "+*.:"
-    coroutines = [
-        blink(canvas, x(), y(), random.choice(symbols)) for _ in range(STARS_COUNT)
-    ]
-    return coroutines
+    def __init__(self, canvas):
+        super().__init__(canvas)
+        self.symbols = "+*.:"
+
+    def make_random_coordinates(self) -> tuple[callable, callable]:
+        """Make random star coordinates."""
+        x, y = tuple(
+            partial(random.randint, 1, limit - 2) for limit in self.border_limits
+        )
+        return x, y
+
+    async def animate(self, row, column, symbol="*") -> None:
+        """Display animation of star."""
+        while True:
+            self.canvas.addstr(row, column, symbol, curses.A_DIM)
+            await wait_for_queue(20 + random.randint(0, 2))
+
+            self.canvas.addstr(row, column, symbol)
+            await wait_for_queue(3 + random.randint(0, 2))
+
+            self.canvas.addstr(row, column, symbol, curses.A_BOLD)
+            await wait_for_queue(5 + random.randint(0, 2))
+
+            self.canvas.addstr(row, column, symbol)
+            await wait_for_queue(3 + random.randint(0, 2))
+
+    def create_event_loop(self) -> list[Coroutine]:
+        """Create a star event loop."""
+        x, y = self.make_random_coordinates()
+        stars_event_loop = [
+            self.animate(x(), y(), random.choice(self.symbols))
+            for _ in range(STARS_COUNT)
+        ]
+        return stars_event_loop
 
 
-def draw(canvas) -> None:
-    """Draw the stars."""
+class SpaceShot(SpaceObject):
+    """Class to make the space shot."""
 
+    def __init__(self, canvas):
+        super().__init__(canvas)
+
+    async def animate(self, row, column, rows_speed=-1, columns_speed=0) -> None:
+        """Display animation of gun shot, direction and speed can be specified."""
+        self.canvas.addstr(round(row), round(column), "*")
+        curses.beep()
+        await asyncio.sleep(0)
+
+        self.canvas.addstr(round(row), round(column), "O")
+        await asyncio.sleep(0)
+
+        self.canvas.addstr(round(row), round(column), " ")
+        row += rows_speed
+        column += columns_speed
+        symbol = "-" if columns_speed else "|"
+        rows, columns = self.border_limits
+        max_row, max_column = rows - 1, columns - 1
+
+        while 0 < row < max_row and 0 < column < max_column:
+            self.canvas.addstr(round(row), round(column), symbol)
+            await asyncio.sleep(0)
+            self.canvas.addstr(round(row), round(column), " ")
+            row += rows_speed
+            column += columns_speed
+
+    def create_event_loop(self) -> list[Coroutine]:
+        """Create a shot event loop."""
+        x, y = self.border_limits
+        fire_coroutine = self.animate(x - 2, y / 2)
+        return [fire_coroutine]
+
+
+class EventLoop:
+    """Class to create common event loop."""
+
+    def __init__(self, canvas, *args):
+        self.sky_star = SkyStar(canvas)
+        self.space_shot = SpaceShot(canvas)
+
+    def create_generic_event_loop(self) -> list[Coroutine]:
+        """Create event loop of all space objects."""
+        final_event_loop = []
+        for event_loop in self.__dict__.values():
+            loop_to_add = event_loop.create_event_loop()
+            final_event_loop.extend(loop_to_add)
+        return final_event_loop
+
+
+def main(canvas) -> None:
+    """Draw the hole picture."""
     canvas.refresh()
     canvas.border()
-    coroutines = make_coroutines(canvas)
-    time_to_wait: int or float = 0.5  # default value, but it will change further
+    event_loop = EventLoop(canvas).create_generic_event_loop()
 
     while True:
-        for coroutine in coroutines.copy():
+        time_start = time.perf_counter()
+        for coroutine in event_loop.copy():
             try:
-                wait = coroutine.send(None)
+                coroutine.send(None)
             except StopIteration:
-                coroutines.remove(coroutine)
-                print("That is all")
+                event_loop.remove(coroutine)
             else:
-                time_to_wait = wait.seconds
                 canvas.refresh()
 
-        time.sleep(time_to_wait)
+        time_stop = time.perf_counter()
+        elapsed_time = time_stop - time_start
+        tic_timeout = TIC_TIMEOUT - elapsed_time
 
-        if len(coroutines) == 0:
-            print("Len == 0")
-            time.sleep(2)
-            break
+        time.sleep(tic_timeout)
 
 
 if __name__ == "__main__":
     curses.update_lines_cols()
     curses.initscr()
     curses.curs_set(0)
-    curses.wrapper(draw)
+    curses.wrapper(main)
